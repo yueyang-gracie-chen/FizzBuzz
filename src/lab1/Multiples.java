0b192671566a716308cf80e166f12397c5ad4574
Gracie Chen <chenyueyanggracie@gmail.com>
2025-09-09 22:12:18 -0400

    change multiples to public

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -5,7 +5,7 @@ public class Multiples {
         System.out.println(multiples(1000, 3, 5));
     }
 
-    static int multiples(int n, int a, int b) {
+    public static int multiples(int n, int a, int b) {
         int count = 0;
         for(int i=1; i<n; i++) {
             if(i % a == 0 || i % b == 0){
